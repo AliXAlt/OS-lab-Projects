728945a4c462f9ddf9e35f41d4b1aa15a6730313
AliXAlt <ali.sadeghi1452@gmail.com>
2024-06-01 23:57:25 +0330

    very final details

--- peterson.py
+++ peterson.py
@@ -14,17 +14,17 @@ def increment(txt,I,J,inc):#(txt is thread IDname, I is thread ID,J is other thr
         while flag[J]==True and turn == J:
             pass
         #Critical section:
-        print(txt + ">>entered.")
+        print(txt + " entered>>")
         counter += 1
-        print(txt + str(counter))
-        print(txt + "<<exited.\n----------")
+        print(txt+ ": " + str(counter))
+        print(txt + " exited<<\n----------")
         #Exit section:
         flag[I]=False
-n1=40
-n2=60
+n1=20
+n2=30
 #Define two thread to target increament function an give them arguments: 
-T1=Thread(target=increment, args=("A: ",0,1,n1))
-T2=Thread(target=increment, args=("B: ",1,0,n2))
+T1=Thread(target=increment, args=("A",0,1,n1))
+T2=Thread(target=increment, args=("B",1,0,n2))
 #Starting threads:
 T1.start()
 T2.start()
